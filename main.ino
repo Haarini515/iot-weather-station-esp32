9adb002a2f242eb62909bc9f3fb87c6a58e95caf
Haarini515 <haarinisureshkumar@gmail.com>
2026-06-23 18:03:30 +0530

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,26 @@
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.println(" %");
+
+  delay(2000);
+}
